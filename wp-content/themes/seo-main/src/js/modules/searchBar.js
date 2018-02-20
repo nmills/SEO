fb535d4b1f265e23e17a1d7d06e02fe1eb075e0a
mohomedanees <anees@thesmallaxxe.com>
2018-02-20 13:53:41 +0530

    Search with js done and img component

---
+++ wp-content/themes/seo-main/src/js/modules/searchBar.js
@@ -0,0 +1,22 @@
+/**
+* Search Bar
+*/
+
+export default function() {
+  var searchForm = $('.c-searchform');
+  var searchBtn = $('.c-search-btn');
+  var searchBtnClose = $('.c-searchform__close-btn');
+
+
+  if(searchForm.length) {
+    searchBtn.click(function (e) {
+      e.preventDefault();
+      searchForm.addClass('active');
+    });
+
+    searchBtnClose.click(function (e){
+      e.preventDefault();
+      searchForm.removeClass('active');
+    })
+  }
+}
