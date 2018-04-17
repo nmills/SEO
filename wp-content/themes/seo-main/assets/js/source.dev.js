5ee5afd23ed0073fc4d87ab5324490fd89b0d292
mohomedanees <anees@thesmallaxxe.com>
2018-04-18 01:14:35 +0530

    Search results page search popup functionality and styles.

--- wp-content/themes/seo-main/assets/js/source.dev.js
+++ wp-content/themes/seo-main/assets/js/source.dev.js
@@ -2698,10 +2698,12 @@ console[method]=noop;
 
 
 function(){
-var searchForm=$('.c-searchform');
+var searchForm=$('.c-searchform--header');
+var searchFormResults=$('.c-searchform--results');
 var searchBtn=$('.js-search-btn');
-var searchBtnClose=$('.c-searchform__close-btn');
-
+var searchBtnResults=$('.js-search-filter__btn');
+var searchBtnClose=$('.c-searchform--header .c-searchform__close-btn');
+var searchBtnCloseResults=$('.c-searchform--results .c-searchform__close-btn');
 
 if(searchForm.length){
 searchBtn.click(function(e){
@@ -2712,6 +2714,18 @@ searchForm.addClass('active');
 searchBtnClose.click(function(e){
 e.preventDefault();
 searchForm.removeClass('active');
+});
+}
+
+if(searchFormResults.length){
+searchBtnResults.click(function(e){
+e.preventDefault();
+searchFormResults.addClass('active');
+});
+
+searchBtnCloseResults.click(function(e){
+e.preventDefault();
+searchFormResults.removeClass('active');
 });
 }
 };
