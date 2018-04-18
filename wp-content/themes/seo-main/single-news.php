1cbd9bfaded20179ac6a53a2228fbef7676ad185
Amalan Premkumar <amalanpremkumar@gmail.com>
2018-04-18 18:03:33 +0530

    This needs to be there on the php files.

--- wp-content/themes/seo-main/single-news.php
+++ wp-content/themes/seo-main/single-news.php
@@ -15,19 +15,18 @@ $context['post'] = $post;
 /* Wiring Recent News and events 
 	Make sure featured post and current post does not appear in the recent posts
 */
-
-// $args = array(
-//   'post_type' => 'news',
-//   'posts_per_page'=> 3,
-//   'post__not_in' => array ($post->ID),
-//   'meta_query' => array(
-// 		array(
-// 			'key' => '_is_ns_featured_post',
-// 			'compare' => 'NOT EXISTS'
-// 		)
-// 	),
-//   'order' => 'DESC'
-// );
-// $context['recent_posts'] = Timber::get_posts($args);
+$args = array(
+  'post_type' => 'news',
+  'posts_per_page'=> 3,
+  'post__not_in' => array ($post->ID),
+  'meta_query' => array(
+		array(
+			'key' => '_is_ns_featured_post',
+			'compare' => 'NOT EXISTS'
+		)
+	),
+  'order' => 'DESC'
+);
+$context['recent_posts'] = Timber::get_posts($args);
 
 Timber::render( $templates, $context );
