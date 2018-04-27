9a6287e2125e49b9220bea0f7685c9ae6b5fcc87
Ahamed Aaqil <aaqil@thesmallaxe.com>
2018-04-27 10:02:03 +0530

    Adjusting the position

--- wp-content/themes/seo-main/assets/js/source.dev.js
+++ wp-content/themes/seo-main/assets/js/source.dev.js
@@ -2716,8 +2716,8 @@ var width=$(window).width();
 if(width>=1024){
 if($('.main-nav-dropdown').length){
 var active_parent=$('a.active-parent').offset().left;
-var container=$('.container.region_hero').offset().left;
-var left_offset=active_parent-container-24;
+var container=$('.main-nav-dropdown').offset().left;
+var left_offset=active_parent-container-32;
 
 console.log(left_offset);
 
--- wp-content/themes/seo-main/src/js/modules/navigation.js
+++ wp-content/themes/seo-main/src/js/modules/navigation.js
@@ -11,8 +11,8 @@ export default function() {
   if ((width >= 1024)) {
     if ($('.main-nav-dropdown').length) {
       var active_parent = $('a.active-parent').offset().left;
-      var container = $('.container.region_hero').offset().left;
-      var left_offset = active_parent - container - 24;
+      var container = $('.main-nav-dropdown').offset().left;
+      var left_offset = active_parent - container - 32;
 
       console.log(left_offset);
 
