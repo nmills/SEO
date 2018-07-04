37f47c4ffd90e38f8d14312afaeee8d64aa24c42
mohomedanees <anees@thesmallaxxe.com>
2018-07-04 11:41:03 +0530

    SEOC-220 (fix-test)

--- wp-content/themes/seo-main/src/js/modules/customDropdown.js
+++ wp-content/themes/seo-main/src/js/modules/customDropdown.js
@@ -2,19 +2,19 @@
 'use strict';
 
 const cusDropdown = function() {
+  var selector = $(".selector");
+  
   if($(window).width() < 767) {
-    $(".selector").on('click', function(){
-      $(".selector").next().removeClass("active");
-      $(this).parent().addClass("active");
+    selector.on('click', function(){
+      selector.next().removeClass("active");
+      $(this).parent().toggleClass("active");
       $(this).toggleClass("show");
-      $(this).next().toggleClass("active");
     });
 
     $(window).on('click', function(e) {
-      if ($(".selector").has(event.target).length == 0 && !$(".selector").is(event.target)){
-        $(".selector").parent().removeClass("active");
-        $(".selector").removeClass("show");
-        $(".selector").next().removeClass("active");
+      if (selector.has(event.target).length == 0 && !selector.is(event.target)){
+        selector.parent().removeClass("active");
+        selector.removeClass("show");
       }
     });
   }
